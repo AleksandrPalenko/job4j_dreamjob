e4ab534ac6b45ab5d71bfd8cb3d9008bdd53c848
Александр <palenko.as@mail.ru>
2022-04-11 20:48:24 +0300

    5. Список Вакансий [#504842 #287257]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -1,4 +1,4 @@
-package ru.job4j.vacancy.model;
+package ru.job4j.dreamjob.model;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -8,15 +8,14 @@ public class Candidate {
     private int id;
     private String name;
     private String description;
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-    private String created = LocalDateTime.now().format(formatter);
+    private LocalDateTime created;
 
     public Candidate(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public Candidate(int id, String name, String description, String created) {
+    public Candidate(int id, String name, String description, LocalDateTime created) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -37,6 +36,22 @@ public class Candidate {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
     }
 
     @Override
