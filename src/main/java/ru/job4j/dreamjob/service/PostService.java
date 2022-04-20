49cb09c4c80343594f0e3f0fb7bc2001fcfccf6a
Александр <palenko.as@mail.ru>
2022-04-20 13:35:34 +0300

    4. Многопользовательские приложения. Многопоточность в Веб приложении. [#504857]

--- src/main/java/ru/job4j/dreamjob/service/PostService.java
+++ src/main/java/ru/job4j/dreamjob/service/PostService.java
@@ -3,10 +3,12 @@ package ru.job4j.dreamjob.service;
 import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.PostStore;
+import net.jcip.annotations.ThreadSafe;
 
 import java.util.Collection;
 
 @Service
+@ThreadSafe
 public class PostService {
 
     private final PostStore store;
