430058ed812aa52b3319a646ab083c016be8d3b5
Александр <palenko.as@mail.ru>
2022-04-29 12:51:37 +0300

    4. Многопоточность в базе данных [#504860 #294869]

--- src/main/java/ru/job4j/dreamjob/service/UserService.java
+++ src/main/java/ru/job4j/dreamjob/service/UserService.java
@@ -5,10 +5,7 @@ import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.model.User;
 import ru.job4j.dreamjob.store.UserDbStore;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @ThreadSafe
@@ -16,14 +13,11 @@ public class UserService {
 
     private final UserDbStore store;
 
-    private final Map<Integer, User> users = new HashMap<Integer, User>();
-    private final AtomicInteger id = new AtomicInteger();
-
     public UserService(UserDbStore store) {
         this.store = store;
     }
 
     public Optional<User> add(User user) {
-        return Optional.ofNullable(users.putIfAbsent(id.incrementAndGet(), user));
+        return Optional.of(store.add(user));
     }
 }
